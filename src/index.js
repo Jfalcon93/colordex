eb6d061fa5113ff713f7327c042e2691dca729d1
Jordan Falcon <jordanfalcon@jordans-mbp.home.gateway>
2019-11-30 16:53:20 -0600

    Trying to Heroku

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-http.createServer(onRequest).listen(process.env.PORT || 6000)
-
 ReactDOM.render(<App />, document.getElementById('app'))
