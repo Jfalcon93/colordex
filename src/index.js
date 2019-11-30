ecf75b314a060106153ce73fa52cfeb7da2c74bf
Jordan Falcon <jordanfalcon@jordans-mbp.home.gateway>
2019-11-30 16:33:51 -0600

    update package

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+http.createServer(onRequest).listen(process.env.PORT || 6000)
+
 ReactDOM.render(<App />, document.getElementById('app'))
